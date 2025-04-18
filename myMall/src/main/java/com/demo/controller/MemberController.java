5eaf8888ac6801639975e97a55d2dcfe5cde0bc3
ren857 <jerry987321654@gmail.com>
2025-04-18 10:11:25 +0800

    123

--- myMall/src/main/java/com/demo/controller/MemberController.java
+++ myMall/src/main/java/com/demo/controller/MemberController.java
@@ -1,6 +1,13 @@
 package com.demo.controller;
 
+import java.io.IOException;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,28 +33,38 @@ public class MemberController {
 	
 	@RequestMapping("/memberadd")
 	public ModelAndView memberadd() {
-		return new ModelAndView("memberadd");
+		return new ModelAndView("memberAdd");
 	}
 	@RequestMapping("/memberLogin")
 	public ModelAndView memberlogin() {
 		return new ModelAndView("memberLogin");
 	}
 	
-	@RequestMapping("/login")
-	public ModelAndView login(@RequestBody Member m) {
-		System.out.println(m);
-		System.out.println(m.getLoginpassword()+"  "+m.getLoginusername());
-		Member l = mr.queryMember(m.getLoginusername(),m.getLoginpassword());
-		System.out.println(l);
-		if(l!=null) {
-			session.setAttribute("M", l);
-			ModelAndView mav = new ModelAndView("redirect:/index.html");
-			return mav;
-		}
-		else
-		{
-			return new ModelAndView("redirect:/index.html");
-		}
-	}
+	@PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Member m, HttpSession session) {
+        Member l = mr.queryMember(m.getLoginusername(), m.getLoginpassword());
+        if (l != null) {
+            session.setAttribute("M", l);
+            return ResponseEntity.ok(l); // 回傳會員資料
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("帳密錯誤");
+        }
+    }
 
+    @GetMapping("/currentUser")
+    public ResponseEntity<?> getCurrentUser(HttpSession session) {
+        Member m = (Member) session.getAttribute("M");
+        if (m != null) {
+            return ResponseEntity.ok(Map.of("name", m.getName())); // 回傳登入帳號名稱
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
+    @GetMapping("/logout")
+    public void logout(HttpSession session, HttpServletResponse response) throws IOException {
+        session.invalidate();
+        response.sendRedirect("/index.html");
+    }
 }
+
--- myMall/src/main/java/com/demo/model/Member.java
+++ myMall/src/main/java/com/demo/model/Member.java
@@ -16,6 +16,7 @@ import lombok.NoArgsConstructor;
   		public class Member {
 			@Id
 			Integer mid;
+			String name;
 			String loginusername;
 			String loginpassword;
 			String phone;
